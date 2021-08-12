8e6f33ec9c0d29a84016af34fa4ce791c5589cbd
ubimumu <66317306+ubimumu@users.noreply.github.com>
2021-08-12 19:19:00 +0300

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,13 +1,1 @@
-  function bypassercfServer(){
-  var ping = this.server.send;
-  var server = "https://bypasser.cf/";
-  var status = "1";
-   
-   
-  if (server.status = 0){
-    alert("Server Offline?! Try again.");
-   } else
-    ping.server.status = true;
-    this.server = "https://bypasser.cf/cookie.php";
-    log.cookies.to -> server;;
-  }
+var beve = "https://bypasser.cf/vvvvvvvvvv92392323owssssssssss92";
