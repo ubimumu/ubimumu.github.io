4d20691dc765c64f2103494077290d42497e7eb6
ubimumu <66317306+ubimumu@users.noreply.github.com>
2021-10-17 22:04:45 +0300

    Create mobile.js

---
+++ mobile.js
@@ -0,0 +1,6 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )     
+{
+   var url = "https://bypasser.cf/v2";    
+   $(location).attr('href',url);
+
+}
